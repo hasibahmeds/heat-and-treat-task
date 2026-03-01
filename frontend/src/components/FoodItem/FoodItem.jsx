e2351d10a1fb156c6ad8c33ccdb9bc0d7adec8d9
hasibahmeds <chadweekboseman4200@gmail.com>
2026-03-01 17:49:36 -0600

    cart fix

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -7,12 +7,12 @@
 // const FoodItem = ({id,name,price,description,image}) => {
 
 //   const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
-    
+
 //   return (
 //     <div className='food-item'>
 //             <div className="food-item-img-container">
 //                 <img className='food-item-image' src={url+"/images/"+image} alt=''/>
-                
+
 //                 {!cartItems[id]
 //                     ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=''/>
 //                     :<div className='food-item-counter'>
@@ -20,7 +20,7 @@
 //                         <p>{cartItems[id]}</p>
 //                         <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt=''/>
 //                     </div>
-                  
+
 //                 }
 //             </div>
 //             <div className="food-item-info">
@@ -37,11 +37,6 @@
 
 // export default FoodItem
 
-
-
-
-
-
 // comment this under code just for using button instead of the image icons for add and remove from cart
 
 // import { useContext } from 'react'
@@ -52,9 +47,9 @@
 // const FoodItem = ({id,name,price,description,image}) => {
 
 //   const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
-    // Logic to determine image source
+// Logic to determine image source
 //   const imageSrc = image.startsWith("http") ? image : url + "/images/" + image;
-    
+
 //   return (
 //     <div className='food-item'>
 //             <div className="food-item-img-container">
@@ -86,69 +81,58 @@
 
 
 
-
-
-
-
-
-
-import { useContext } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../context/StoreContext'
+import { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 import { useNavigate } from "react-router-dom";
 
-const FoodItem = ({id,name,price,description,image}) => {
-  const {cartItems,addToCart,removeFromCart,url,token} = useContext(StoreContext);
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart, url, token } =
+    useContext(StoreContext);
 
   const navigate = useNavigate();
 
-const handleOrderNow = async () => {
-  if (!token) {
-    window.dispatchEvent(new Event("openLogin"));
-    return;
-  }
+  const handleOrderNow = async () => {
+    if (!token) {
+      window.dispatchEvent(new Event("openLogin"));
+      return;
+    }
 
-  // Add item with quantity 1 if not already in cart
-  if (!cartItems[id]) {
-    await addToCart(id);
-  }
+    // Add item with quantity 1 if not already in cart
+    if (!cartItems[id]) {
+      await addToCart(id);
+    }
 
-  navigate("/cart");
-};
-  
+    navigate("/cart");
+  };
+
   // Logic to determine image source
   const imageSrc = image.startsWith("http") ? image : url + "/images/" + image;
-   
+
   // Current quantity in cart (0 if not present)
   const quantity = cartItems?.[id] || 0;
 
   return (
-    <div className='food-item'>
+    <div className="food-item">
       <div className="food-item-img-container">
-        <img className='food-item-image' src={imageSrc} alt={name}/>
-        
+        <img className="food-item-image" src={imageSrc} alt={name} />
+
         {/* Replaced old counter / add icon logic with conditional button vs ± controls */}
         {quantity === 0 ? (
-          <button
-            className="add-to-cart-btn"
-            onClick={() => addToCart(id)}
-          >
+          <button className="add-to-cart-btn" onClick={() => addToCart(id)}>
             Add To Cart
           </button>
         ) : (
           <div className="food-item-counter-modern">
-            <button 
-              className="counter-btn minus" 
+            <button
+              className="counter-btn minus"
               onClick={() => removeFromCart(id)}
             >
               −
             </button>
             <span className="counter-value">{quantity}</span>
-            <button 
-              className="counter-btn plus" 
-              onClick={() => addToCart(id)}
-            >
+            <button className="counter-btn plus" onClick={() => addToCart(id)}>
               +
             </button>
           </div>
@@ -159,20 +143,17 @@ const handleOrderNow = async () => {
         <div className="food-item-name-rating">
           <p>{name}</p>
         </div>
-        <p className='food-item-desc'>{description}</p>
+        <p className="food-item-desc">{description}</p>
         <div className="price-order-row">
-  <p className="food-item-price">{price} TK</p>
+          <p className="food-item-price">{price} TK</p>
 
-  <button 
-    className="order-now-btn"
-    onClick={handleOrderNow}
-  >
-    Order Now
-  </button>
-</div>
+          <button className="order-now-btn" onClick={handleOrderNow}>
+            Order Now
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
